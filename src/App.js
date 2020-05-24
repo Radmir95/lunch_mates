1b32c17acf8965b40e912a68ce9fa2b0f8238d8f
radmir <v-rakhus@microsoft.com>
2020-05-24 23:10:30 +0300

    add prevew menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.navbarClick = this.navbarClick.bind(this);
+    this.myRef = React.createRef();
+  }
+
+  navbarClick(){
+    console.log(this.myRef.current.classList.toggle('active'));
+  }
+
+  render(){
+    return (
+      <nav className="navbar">
+        <span className="navbar-toggle">
+        <i onClick={this.navbarClick} className="box-shadow-menu"></i>
+        </span>
+    <a className="logo">Lunch Mates</a>
+    <ul ref={this.myRef} className="main-nav">
+      <li>
+        <a className="nav-links">View</a>
+      </li>
+      <li>
+        <a className="nav-links">Schedule new</a>
+      </li>
+      <li>
+        <a className="nav-links">Participation history</a>
+      </li>
+    </ul>
+  </nav>
+   )
+  }
+
 }
 
 export default App;
