079a34febff9bac470765f02ef59a90c614823ea
Radmir <husnutdinovradmir@gmail.com>
2020-10-26 23:13:13 +0300

    Little changes

--- src/Components/Lunch.js
+++ src/Components/Lunch.js
@@ -16,7 +16,7 @@ export default class Lunch extends Component{
             <li>{this.props.lunch.place}</li>
             <li>{this.props.lunch.address}</li>
             <li>{this.props.lunch.datetime}</li>
-            <li className="participantsButton"><a onClick={ e => this.expandOrCloseParticipants(e.target)}>Participants</a><span className="count blueBackground"> Count: {this.props.lunch.participants.length}</span></li>
+            <li className="participantsButton"><a onClick={ e => this.expandOrCloseParticipants(e.target)}>Participants</a><span className="count blueBackground">count: {this.props.lunch.participants.length}</span></li>
             <li className="participants">
                 <ul className="participant">{this.props.lunch.participants.map((participant, key)=>{
                     return <li key={key}>{participant}</li>
