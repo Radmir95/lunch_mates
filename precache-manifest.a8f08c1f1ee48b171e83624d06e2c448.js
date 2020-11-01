dc278660a01617fbf92f27285dc192e64e0ed639
Radmir <husnutdinovradmir@gmail.com>
2020-11-01 17:32:53 +0300

    Updates

---
+++ precache-manifest.a8f08c1f1ee48b171e83624d06e2c448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ffe55cf09bbc29cbf3cff0f57f46a2a",
+    "url": "/lunch_mates/index.html"
+  },
+  {
+    "revision": "c9bf16a9a520d4fcd551",
+    "url": "/lunch_mates/static/css/main.9f6e47db.chunk.css"
+  },
+  {
+    "revision": "8b38b201cecc301703ad",
+    "url": "/lunch_mates/static/js/2.88beefb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lunch_mates/static/js/2.88beefb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9bf16a9a520d4fcd551",
+    "url": "/lunch_mates/static/js/main.aef77ada.chunk.js"
+  },
+  {
+    "revision": "1a72f6215d87fdd0dcc4",
+    "url": "/lunch_mates/static/js/runtime-main.6145b5da.js"
+  }
+]);
